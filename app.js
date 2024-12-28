0fa8e0cdb0b2acfb90254dc74b551f449ec4e2ec
Pratham <120655103+s-pratham@users.noreply.github.com>
2024-12-28 14:18:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ app.post('/stockAnalysisSearch', async function (req, res) {
 
         // Call the Flask backend for predictions
         const axios = require('axios'); // Import axios for making API calls
-        const flaskURL = 'https://2cd4-2409-40c2-b-1c-144b-b979-f2e9-27ff.ngrok-free.app/predict'; // Flask backend URL
+        const flaskURL = 'https://830a-2409-40c2-3001-ac1b-916e-322b-3427-2aaa.ngrok-free.app/predict'; // Flask backend URL
         
         const predictionResponse = await axios.post(flaskURL, {
             item: id // Send item ID to the Flask backend
